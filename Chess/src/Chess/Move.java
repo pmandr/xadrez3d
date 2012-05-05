da6d270785f64a0f0edafbf71faf0792f28027cf
lucaslobosque <lucaslobosque@gmail.com>
2012-05-05 21:41:24 +0000

    Possíveis movimentos do cavalo + testes.

---
+++ Chess/src/Chess/Move.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chess;
+
+/**
+ *
+ * @author Lobosque
+ */
+public class Move {
+    private Square from;
+    private Square to;
+}
---
+++ Chess/src/Chess/exception/InvalidSquareException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chess.exception;
+
+/**
+ *
+ * @author Lobosque
+ */
+public class InvalidSquareException extends ChessException {
+      public InvalidSquareException() {}
+
+}
