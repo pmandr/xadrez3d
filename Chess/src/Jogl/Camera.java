95ffba661f1c79bd2d749b414971d8e1984498e0
paulomariomariomario@gmail.com <paulomariomariomario@gmail.com@d0ff2ef1-2552-22a1-9ffb-2b8093973c23>
2012-05-06 14:14:24 +0000

    reposicionamento/dimensionamento de tabuleiro; alguns controles de camera;

--- Chess/src/Jogl/Camera.java
+++ Chess/src/Jogl/Camera.java
@@ -5,6 +5,8 @@
 package Jogl;
 
 import java.awt.Point;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.media.opengl.GL;
 import javax.media.opengl.glu.GLU;
 
@@ -13,19 +15,20 @@ public class Camera {
     private float PosX;
     private float PosZ;
     private float PosY;
-    private float EyeX;
-    private float EyeZ;
-    private float EyeY;
+    private float LookAtPointX;
+    private float LookAtPointY;
+    private float LookAtPointZ;
     private float UpX;
     private float UpY;
     private float UpZ;
 
     //Essas variaveis aqui são públicas pq fiz correndo! Usar private com getter/setter!
 
-    public float rot;
-    public float pitch;
-    public float lastx;
-    public float lasty;
+    private float rot;
+    private float pitch;
+    private  float lastx;
+    private float lasty;
+    private int player;
 
 
 
@@ -41,16 +44,16 @@ public class Camera {
         return PosZ;
     }
 
-    public float getEyeX() {
-        return EyeX;
+    public float getLookAtPointX() {
+        return LookAtPointX;
     }
 
-    public float getEyeY() {
-        return EyeY;
+    public float getLookAtPointY() {
+        return LookAtPointZ;
     }
 
-    public float getEyeZ() {
-        return EyeZ;
+    public float getLookAtPointZ() {
+        return LookAtPointY;
     }
 
     public float getUpX() {
@@ -66,18 +69,8 @@ public class Camera {
     }
 
     public Camera() {
-        EyeX = -1.5f;
-        EyeY = -1.5f;
-        EyeZ = -6.0f;
-
-        PosX = 40.0f;
-        PosY = 40.0f;
-        PosZ = 0.0f;
-
-        UpX = 0;
-        UpY = 1;
-        UpZ = 0;
-
+        loadPlayer1View();
+        player=1;
         lastx = 0;
         lasty = 0;
 
@@ -85,51 +78,91 @@ public class Camera {
         rot = (float) (Math.PI / 2);
 
     }
+    
+    //a visao do jogador 1 eh a do lado das pecas brancas
+    public void loadPlayer1View(){
+        LookAtPointX = 8.0f;
+        LookAtPointZ = 0.0f;
+        LookAtPointY = 8.0f;
+
+        PosX = -16.0f;
+        PosY = 16.0f;
+        PosZ = 8.0f;
+
+        UpX = 0;
+        UpY = 1;
+        UpZ = 0;
+    }
+    public void loadPlayer2View(){
+        LookAtPointX = 8.0f;
+        LookAtPointZ = 0.0f;
+        LookAtPointY = 8.0f;
+        
+        PosX = 32.0f;
+        PosY = 16.0f;
+        PosZ = 8.0f;
+
+        UpX = 0;
+        UpY = 1;
+        UpZ = 0;
+    }
+    public void changePlayerView(){
+        if(player==1){
+            loadPlayer2View();
+            player=2;
+        }else{
+            loadPlayer1View();
+            player=1;
+        }
+    }
 
     public void moveFront() {
-        this.EyeX += (Math.cos(this.rot) * Math.cos(this.pitch)) / 2;
-        this.EyeY += -(Math.sin(this.rot) * Math.cos(this.pitch)) / 2;
-        this.EyeZ += Math.sin(this.pitch);
+//        this.EyeX += (Math.cos(this.rot) * Math.cos(this.pitch)) / 2;
+//        this.EyeY += -(Math.sin(this.rot) * Math.cos(this.pitch)) / 2;
+//        this.EyeZ += Math.sin(this.pitch);
+        this.LookAtPointX+=1;
         this.update();
     }
 
     public void moveBack() {
-        this.EyeX -= (Math.cos(this.rot) * Math.cos(this.pitch)) / 2;
-        this.EyeY -= -(Math.sin(this.rot) * Math.cos(this.pitch)) / 2;
-        this.EyeZ -= Math.sin(this.pitch);
+//        this.EyeX -= (Math.cos(this.rot) * Math.cos(this.pitch)) / 2;
+//        this.EyeY -= -(Math.sin(this.rot) * Math.cos(this.pitch)) / 2;
+//        this.EyeZ -= Math.sin(this.pitch);
+        this.LookAtPointZ+=1;
         this.update();
     }
 
     public void strafeLeft() {
-        this.EyeX += Math.cos(this.rot + Math.PI / 2) / 2;
-        this.EyeZ += -Math.sin(this.rot + Math.PI / 2) / 2;
+        this.LookAtPointX += Math.cos(this.rot + Math.PI / 2) / 2;
+        this.LookAtPointY += -Math.sin(this.rot + Math.PI / 2) / 2;
         this.update();
     }
 
     public void strafeRight() {
-        this.EyeX -= Math.cos(this.rot + Math.PI / 2) / 2;
-        this.EyeZ -= -Math.sin(this.rot + Math.PI / 2) / 2;
+        this.LookAtPointX -= Math.cos(this.rot + Math.PI / 2) / 2;
+        this.LookAtPointY -= -Math.sin(this.rot + Math.PI / 2) / 2;
         this.update();
     }
 
     public void moveUp() {
-        this.EyeX += (Math.cos(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
-        this.EyeY += -(Math.sin(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
-        this.EyeZ += Math.sin(this.pitch);
+//        this.EyeX += (Math.cos(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
+//        this.EyeY += -(Math.sin(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
+//        this.EyeZ += Math.sin(this.pitch);
+        this.LookAtPointY+=1;
         this.update();
     }
 
     public void moveDown() {
-        this.EyeX -= (Math.cos(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
-        this.EyeY -= -(Math.sin(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
-        this.EyeZ -= Math.sin(this.pitch);
+        this.LookAtPointX -= (Math.cos(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
+        this.LookAtPointZ -= -(Math.sin(this.rot) * Math.cos(this.pitch + Math.PI / 2)) / 2;
+        this.LookAtPointY -= Math.sin(this.pitch);
         this.update();
     }
 
     public void update() {
-        PosX = (float) (EyeX + (Math.cos(rot) * Math.cos(pitch)));
-        PosZ = (float) (EyeZ - (Math.sin(rot) * Math.cos(pitch)));
-        PosY = (float) (EyeY + Math.sin(pitch));
+        PosX = (float) (LookAtPointX + (Math.cos(rot) * Math.cos(pitch)));
+        PosZ = (float) (LookAtPointY - (Math.sin(rot) * Math.cos(pitch)));
+        PosY = (float) (LookAtPointZ + Math.sin(pitch));
 
         UpX = (float) (Math.cos(rot) * Math.cos((pitch + Math.PI / 2)));
         UpZ = (float) (-Math.sin(rot) * Math.cos((pitch + Math.PI / 2)));
